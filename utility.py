8f85609a539ece27273aa50521fc241b23437c0a
thomasmorgan <thomas.j.h.morgan@gmail.com>
2018-04-11 10:09:53 -0700

    add haversine function to utility

--- utility.py
+++ utility.py
@@ -1,6 +1,24 @@
 """Various utility methods."""
 
 import settings
+import numpy as np
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    r = settings.world_radius
+    return c * r
 
 
 def log(str, force=False):
